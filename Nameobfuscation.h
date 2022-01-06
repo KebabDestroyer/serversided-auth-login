f36d824c0e6f3750de00050f21c22f72b5239441
whatisthat928 <97236321+whatisthat928@users.noreply.github.com>
2022-01-06 10:32:26 -0330

    Create Nameobfuscation.h

---
+++ Nameobfuscation.h
@@ -0,0 +1,7 @@
+//Obfuscating the names
+//Change them to anything 
+//Make sure you also change them inside APIClient.h 
+//This will help slow down people who want to crack the tweak, by no means it's a bypass proof solution, every auth can be cracked  
+#define APIClient Ka82828jamama
+#define paid kaKaka9191
+#define free kama919191
